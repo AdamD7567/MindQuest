6caaee1005720b3defd85c257ae5f560cd2a12d8
Adam Imane <adamdeardon@hotmail.com>
2025-11-01 16:43:27 +0000

    Finished up the foundation for header concept 1

---
+++ header1/header1.js
@@ -0,0 +1,6 @@
+function onMenuClick() {
+    let navbar = document.getElementById("navigation-bar");
+    let responsive_class_name = "responsive";
+
+    navbar.classList.toggle(responsive_class_name);
+}
