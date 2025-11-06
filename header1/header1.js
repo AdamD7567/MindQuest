6a31843e36d8ac461988f86cf11136f72c100e33
Adam Imane <adamdeardon@hotmail.com>
2025-11-06 17:59:33 +0000

    NOTE: Forgot to push commits to main, reminder to do this more often.
    Changes:
    - Integrated team's sidebar with all sections.

--- header1/header1.js
+++ header1/header1.js
@@ -1,13 +1,13 @@
 // Toggle sidebar animation
 function toggleSidebar() {
-    let navbar = document.getElementById('navigation-bar');
-    navbar.classList.toggle('closed');
+    let sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("closed");
 }
 
 // Hide sidebar automatically when zooming out.
 window.addEventListener('resize', () => {
-    let navbar = document.getElementById('navigation-bar');
-    if (window.innerWidth > 1250 && !navbar.classList.contains('closed')) {
-        navbar.classList.add('closed');
+    let sidebar = document.getElementById('sidebar');
+    if (window.innerWidth > 1250 && !sidebar.classList.contains('closed')) {
+        sidebar.classList.add('closed');
     }
 });
